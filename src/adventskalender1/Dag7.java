0524e3f178fd1b1c143fef5abe62923c2871a051
Robin Chatfield <robin.chatfield@gmail.com>
2016-02-23 10:54:49 +0100

    Fixade ej helt borttagen konflikt. Fixade trasigt MS-idiot-tecken-som-förstör-UTF8-filer.

--- src/adventskalender1/Dag7.java
+++ src/adventskalender1/Dag7.java
@@ -1,25 +1,13 @@
-﻿package adventskalender1;
-
+package adventskalender1;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-
 class Dag7 {
-
-
     public static void Lucka7 () {
-<<<<<<< HEAD
         String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-
-
         ImageIcon ikon = new ImageIcon(Dag7.class.getResource("/adventskalender1/julgran.png"));
         JOptionPane.showMessageDialog(Index.getInstance(), "07 December\nKlockan är: " + timeStamp,
                 "Lucka 07", JOptionPane.INFORMATION_MESSAGE, ikon);
-
     }
-
-    }
-
 }
